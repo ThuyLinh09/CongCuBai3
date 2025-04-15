6858c8e44d827e64d82464e66887007a13bc45ea
ThuyLinh09 <TieuHoLy.09>
2025-04-15 20:34:57 +0700

    new

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Test() {
+	fmt.Println("Test function")
+
+}
+
+func Subtract(a, b int) int {
+	return a - b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
+
+func IsPrime(n int) bool {
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return n > 1
+}
+func main() {
+	fmt.Println("Hello, World!")
+}
