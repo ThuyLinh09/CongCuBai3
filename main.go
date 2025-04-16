beb64221c29e9e6fb80ea430cf595979c07b48f3
ThuyLinh09 <TieuHoLy.09>
2025-04-16 11:32:17 +0700

    new

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import "fmt"
 
 
 func IsPrime(n int) bool {
-	return null
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return n > 1
 }
 func main() {
 	fmt.Println("Hello, World!")
